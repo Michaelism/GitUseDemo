d732b001c2f6f9d50982d72c7a24774102a924bc
Michael Wong <michaelwong@dyn-118-138-90-165.its.monash.edu.au>
2018-09-10 15:29:49 +1000

    Add a map

---
+++ GitUseDemo/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseDemo
+//
+//  Created by Michael Wong on 10/9/18.
+//  Copyright © 2018 MWstudio. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+
+}
